a12a2cfaa371bebfdac4c846e7c3227488e541d5
Felippe Pires <felippepg.goncalves@outlook.com.br>
2021-10-20 06:53:03 -0300

    Configurando projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./App.module.scss"
+
+function App() {
+  return (
+    <main className={styles.contentWrapper}>
+      <h1>Olá</h1>
+    </main>
+  )
+}
+
+export { App }
