9cb7e06df1c705a12218b139c1ae2b2e04d232a8
Felippe Pires <felippepg.goncalves@outlook.com.br>
2021-10-20 21:12:31 -0300

    Integração com as mensagens do backend

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,41 +1,44 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import logoImg from "../../assets/logo.svg"
 import styles from "./style.module.scss"
+import { api } from "../../services/api"
+
+interface IMessage {
+  id: string
+  text: string
+  user: {
+    avatar_url: string
+    name: string
+  }
+}
 
 export function MessageList() {
+  const [messages, setMessages] = useState<IMessage[]>([])
+
+  useEffect(() => {
+    api.get<IMessage[]>('messages/last3').then(response => 
+      setMessages(response.data)
+    )
+  }, [])
+
   return (
     <div className={styles.messageBoxWrapper}>
       <img src={logoImg} alt="Logo NWL Heat" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/felippepg.png" alt="Usuario" />
-            </div>
-            <span>Felippe Pires</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/felippepg.png" alt="Usuario" />
-            </div>
-            <span>Felippe Pires</span>
-          </div>
-        </li>
-        
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/felippepg.png" alt="Usuario" />
-            </div>
-            <span>Felippe Pires</span>
-          </div>
-        </li>
+        {messages.map(message => {
+          return(
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
